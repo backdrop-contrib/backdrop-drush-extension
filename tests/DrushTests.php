a7409b732b24208ad25ac327d3c23bed23835776
Geoff St. Pierre <serundeputy@gmail.com>
2019-06-22 15:49:42 -0400

    Document functions and code clean up.

--- tests/DrushTests.php
+++ tests/DrushTests.php
@@ -1,22 +1,33 @@
 <?php
+/**
+ * @file
+ * Initial test suite for Backdrop Drush Extension.
+ */
+
 use PHPUnit\Framework\TestCase;
 
 class DrushTests extends TestCase {
+  /**
+   * Test drush st command.
+   */
   public function testDrushSt() {
     exec('drush st', $output);
     $this->assertStringContainsString('Backdrop version', $output[0]);
     $this->assertStringContainsString('Backdrop bootstrap', $output[7]);
     $this->assertStringContainsString('Successful', $output[7]);
-    $this->assertStringContainsString('PHP OS',$output[10]);
+    $this->assertStringContainsString('PHP OS', $output[10]);
     $this->assertStringContainsString('Backdrop Settings File', $output[18]);
   }
 
+  /**
+   * Test drush dl devel command.
+   */
   public function testPmDownload() {
     exec("drush dl devel -n", $output);
     $this->assertStringContainsString('Success:', $output[1]);
     $this->assertStringContainsString('devel', $output[1]);
     $this->assertDirectoryExists('/app/web/modules/devel');
-    // clean up.
+    // Clean up.
     exec('rm -rf /app/web/modules/devel');
   }
 }
